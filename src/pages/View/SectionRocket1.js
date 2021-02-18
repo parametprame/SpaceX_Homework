5490e7f077e51227686f08bf6c33a657ca8a5366
Denpoom Srimuang <forizzle28@hotmail.com>
2021-02-18 22:10:55 +0700

    FeatureRocket

--- src/pages/View/SectionRocket1.js
+++ src/pages/View/SectionRocket1.js
@@ -10,7 +10,9 @@ const SectionRocket1 = () => {
                     <Container>
                         <div className='textabout' data-aos="fade-up">
                             <p>Rocket</p>
-                            
+                            <h6>SpaceX designs, manufactures and launches advanced rockets and spacecraft.</h6>
+                            <h6>The company was founded in 2002 to revolutionize space technology,</h6>
+                            <h6>with the ultimate goal of enabling people to live on other planets.</h6>
                         </div>
                     </Container>
                 </div>
