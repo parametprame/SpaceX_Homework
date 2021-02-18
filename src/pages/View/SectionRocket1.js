eb2bdae4ea0ded8af2a2598017cacb6b18e9279f
Denpoom Srimuang <forizzle28@hotmail.com>
2021-02-18 22:00:45 +0700

    FeatureRocket

---
+++ src/pages/View/SectionRocket1.js
@@ -0,0 +1,21 @@
+import 'aos/dist/aos.css';
+import React from 'react'
+import {Container} from 'react-bootstrap';
+
+const SectionRocket1 = () => {
+    return(
+        <React.Fragment>
+        <section id='aboutrocket'>
+                <div className="viewaboutrocket">
+                    <Container>
+                        <div className='textabout' data-aos="fade-up">
+                            <p>Rocket</p>
+                            
+                        </div>
+                    </Container>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+export default SectionRocket1
